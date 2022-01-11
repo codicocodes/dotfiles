970e08945e292ee0051c7fc356c30ef258495963
codico <codicocodes@gmail.com>
2022-01-11 23:33:20 +0100

    First try

--- zsh/select_wallpaper.sh
+++ zsh/select_wallpaper.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-selected=`find "/Users/codico/wallpapers" -iname '*.png' -type f | fzf --preview "(kitty icat --place 19x10@55x0 --transfer-mode file {} )"`
+selected=`find "/Users/codico/wallpapers" -iname '*.png' -type f | fzf --preview "(kitty icat --place 60x60@65x6 --transfer-mode file {} )"`
 echo "Setting background to $selected (* ^ ω ^).."
 kitty @ set-background-image $selected
 echo "background_image $selected" > "$HOME/code/dotfiles/kitty/background_image.conf"
