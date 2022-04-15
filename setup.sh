932ce1e09fdd818d44475d0cedafc6f22e3d2983
codico <codicocodes@gmail.com>
2022-04-15 17:18:12 +0200

    Use /home/codico in setup.sh

--- setup.sh
+++ setup.sh
@@ -2,14 +2,14 @@
 
 DOTFILES_PATH="$(cd "$(dirname "$1")" && pwd)/$(basename "$1")"
 
-KITTY_PATH="/Users/codico/code/dotfiles/kitty/"
-VIM_PATH="/Users/codico/code/dotfiles/vim/.vimrc"
-ZSH_PATH="/Users/codico/code/dotfiles/zsh/.zshrc"
-NVIM_PATH="/Users/codico/code/dotfiles/nvim/"
+KITTY_PATH="$HOME/code/dotfiles/kitty/"
+VIM_PATH="$HOME/code/dotfiles/vim/.vimrc"
+ZSH_PATH="$HOME/code/dotfiles/zsh/.zshrc"
+NVIM_PATH="$HOME/code/dotfiles/nvim/"
 
-ln -sf $KITTY_PATH ~/.config
-ln -sf $NVIM_PATH ~/.config
-ln -sf $VIM_PATH ~/.vimrc
+# ln -sf $KITTY_PATH ~/.config
+# ln -sf $NVIM_PATH ~/.config
+# ln -sf $VIM_PATH ~/.vimrc
 ln -sf $ZSH_PATH ~/.zshrc
 
 echo "Symlink Completed"
