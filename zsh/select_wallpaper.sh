b606815f94224c40fcceb1330e48179aba8864d4
codico <codicocodes@gmail.com>
2022-04-15 17:10:43 +0200

    Use /home/codico in select bg script

--- zsh/select_wallpaper.sh
+++ zsh/select_wallpaper.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-selected=`find "/Users/codico/wallpapers" -iname '*.png' -type f | fzf --preview "(kitty icat --place 60x60@65x6 --transfer-mode file {} )"`
+selected=`find "$HOME/wallpapers" -iname '*.png' -type f | fzf --preview "(kitty icat --place 60x60@65x6 --transfer-mode file {} )"`
 echo "Setting background to $selected (* ^ ω ^).."
 echo "background_image $selected" > "$HOME/code/dotfiles/kitty/background_image.conf"
 kitty @ set-background-image $selected
