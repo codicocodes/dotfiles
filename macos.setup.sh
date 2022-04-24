71c8fba3f2070577cb9eb382c1ea1d1de0c6e1bb
codico <codicocodes@gmail.com>
2022-04-24 15:42:58 +0200

    Cleanup setup script

--- macos.setup.sh
+++ macos.setup.sh
@@ -20,7 +20,6 @@ brew install wget --with-iri
 
 # Install more recent versions of some OS X tools
 brew install neovim
-brew install vim --with-override-system-vi
 brew install ripgrep
 
 # Install other useful binaries
@@ -38,15 +37,3 @@ brew install openssl
 
 # Remove outdated versions from the cellar
 brew cleanup
-
-# brew install nvm # This installs `npm` too using the recommended installation method
-# nvm install 16
-# nvm use 16
-
-# npm install -g prettier
-# npm install -g eslint
-# npm install -g ngrok
-# npm install -g typescript
-# npm install -g typescript-language-server
-# npm i -g vscode-langservers-extracted
-# npm i -g vls
