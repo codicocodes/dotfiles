8c3f9fe8687028c7492c9339e4c6a681baa249cc
codico <codicocodes@gmail.com>
2022-09-30 19:46:29 +0200

    Cleanup plugins init

--- nvim/lua/plugins/init.lua
+++ nvim/lua/plugins/init.lua
@@ -1,69 +1,55 @@
-local packer = require'packer'
+local packer = require"packer"
 
 packer.init({})
+
+local troublSetup = function()
+  require"trouble".setup{
+    signs = {
+      error = "",
+      warning = "",
+      hint = "",
+      information = "",
+      other = ""
+    },
+  }
+end
 
 packer.startup(function()
   ---@diagnostic disable-next-line: undefined-global
   local use = use
-  use "phaazon/mind.nvim"
-  use({
-    "glepnir/lspsaga.nvim",
-    branch = "main",
-    config = function()
-      local saga = require("lspsaga")
-      saga.init_lsp_saga({})
-    end,
-  })
-  use "mfussenegger/nvim-jdtls"
+
+  -- Debugger setup
   use "mfussenegger/nvim-dap"
   use "leoluz/nvim-dap-go"
   use "rcarriga/nvim-dap-ui"
-  use "rcarriga/nvim-notify"
   use "theHamsta/nvim-dap-virtual-text"
   use "mfussenegger/nvim-dap-python"
   use "nvim-telescope/telescope-dap.nvim"
+
   use "vim-test/vim-test"
+  use "nvim-treesitter/nvim-treesitter"
+  use "nvim-treesitter/nvim-treesitter-textobjects"
+  use "sheerun/vim-polyglot"
+  use "nvim-lua/popup.nvim"
+  use "nvim-lua/plenary.nvim"
+  use "nvim-lua/telescope.nvim"
+  use "nvim-telescope/telescope-file-browser.nvim"
+  use "jremmen/vim-ripgrep"
+
+  -- LSP
+  use "neovim/nvim-lspconfig"
+  use "j-hui/fidget.nvim"
+  use "jose-elias-alvarez/null-ls.nvim"
+  use "glepnir/lspsaga.nvim"
   use {
-    "folke/todo-comments.nvim",
-    requires = "nvim-lua/plenary.nvim",
-    config = function()
-      require"todo-comments".setup{}
-    end
+    "weilbith/nvim-code-action-menu",
+    cmd = "CodeActionMenu",
   }
   use {
     "folke/trouble.nvim",
     requires = "kyazdani42/nvim-web-devicons",
-    config = function()
-      require"trouble".setup{
-        signs = {
-          error = "",
-          warning = "",
-          hint = "",
-          information = "",
-          other = ""
-        },
-      }
-    end
+    config = troublSetup
   }
-  use {
-    "folke/zen-mode.nvim",
-    config = function()
-      require("zen-mode").setup{}
-    end
-  }
-  use 'nvim-treesitter/nvim-treesitter'
-  use 'nvim-treesitter/nvim-treesitter-textobjects'
-  use 'sheerun/vim-polyglot'
-  use 'nvim-lua/popup.nvim'
-  use 'nvim-lua/plenary.nvim'
-  use 'nvim-lua/telescope.nvim'
-  use "nvim-telescope/telescope-file-browser.nvim"
-  use 'jremmen/vim-ripgrep'
-
-  -- LSP
-  use 'neovim/nvim-lspconfig'
-  use 'j-hui/fidget.nvim'
-  use 'jose-elias-alvarez/null-ls.nvim'
 
   -- Autocomplete
   use {
@@ -85,46 +71,52 @@ packer.startup(function()
     },
   }
 
-  use{
-    'weilbith/nvim-code-action-menu',
-    cmd = 'CodeActionMenu',
-  }
+  use "ThePrimeagen/harpoon"
 
-  use 'ThePrimeagen/harpoon'
   -- THEMES
-  use 'sainnhe/edge'
-  use 'codicocodes/tokyonight.nvim' -- my version of tokyonight is modified for better transparency support
-  use 'arcticicestudio/nord-vim'
+  use "sainnhe/edge"
+  use "codicocodes/tokyonight.nvim" -- my version of tokyonight is modified for better transparency support
+  use "arcticicestudio/nord-vim"
 
   -- GIT
-  use 'TimUntersberger/neogit'
-  use 'tpope/vim-fugitive'
+  use "TimUntersberger/neogit"
+  use "tpope/vim-fugitive"
   use {
-      'lewis6991/gitsigns.nvim',
-      requires = 'nvim-lua/plenary.nvim'
+      "lewis6991/gitsigns.nvim",
+      requires = "nvim-lua/plenary.nvim"
   }
-
 
   -- EDITING
-  use 'jiangmiao/auto-pairs'
-  use { 'prettier/vim-prettier', run = 'yarn install' }
-  use 'tpope/vim-surround'
-  use 'tpope/vim-commentary'
-  use 'phaazon/hop.nvim'
-  use 'tpope/vim-abolish'
+  use "jiangmiao/auto-pairs"
+  use { "prettier/vim-prettier", run = "yarn install" }
+  use "tpope/vim-surround"
+  use "tpope/vim-commentary"
+  use "phaazon/hop.nvim"
+  use "tpope/vim-abolish"
 
   -- EDITOR
-  use 'kyazdani42/nvim-web-devicons'
+  use "kyazdani42/nvim-web-devicons"
   use {
-    'kyazdani42/nvim-tree.lua',
-    requires = 'kyazdani42/nvim-web-devicons',
-    config = function() require'nvim-tree'.setup() end
+    "kyazdani42/nvim-tree.lua",
+    requires = "kyazdani42/nvim-web-devicons",
   }
+  use {
+    "folke/todo-comments.nvim",
+    requires = "nvim-lua/plenary.nvim",
+  }
+  use "folke/zen-mode.nvim"
+
+  -- LIFESTYLE
+  use "phaazon/mind.nvim"
 end
 )
 
-vim.notify = require("notify")
-
-require('gitsigns').setup{
+require("gitsigns").setup{
   signcolumn = false
 }
+
+require"zen-mode".setup{}
+require"todo-comments".setup{}
+require"nvim-tree".setup()
+local saga = require("lspsaga")
+saga.init_lsp_saga({})
