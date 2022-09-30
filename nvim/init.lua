17edec3c0f39451569844aa906f46bcd86a947af
codico <codicocodes@gmail.com>
2022-09-30 19:22:42 +0200

    Move treesitter textobj config

--- nvim/init.lua
+++ nvim/init.lua
@@ -12,22 +12,6 @@ require'plugins.cmp'
 require'fidget'.setup{}
 require'mind'.setup{}
 require'hop'.setup { keys = 'etovxqpdygfblzhckisuran', term_seq_bias = 0.5 }
-require'nvim-treesitter.configs'.setup {
-  textobjects = {
-    select = {
-      enable = true,
-      -- Automatically jump forward to textobj, similar to targets.vim
-      lookahead = true,
-      keymaps = {
-        -- You can use the capture groups defined in textobjects.scm
-        ["af"] = "@function.outer",
-        ["if"] = "@function.inner",
-        ["ac"] = "@class.outer",
-        ["ic"] = "@class.inner",
-      },
-    },
-  },
-}
 
 require("null-ls").setup({
     sources = {
