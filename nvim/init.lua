802fbf9f30310926a6e461b629d38419facc1ad4
codico <codicocodes@gmail.com>
2022-09-30 19:27:21 +0200

    Move debugger logic to DAP config

--- nvim/init.lua
+++ nvim/init.lua
@@ -12,19 +12,10 @@ require'plugins.cmp'
 require'fidget'.setup{}
 require'mind'.setup{}
 require'hop'.setup { keys = 'etovxqpdygfblzhckisuran', term_seq_bias = 0.5 }
-
+require'plugins.dap'
 require("null-ls").setup({
     sources = {
         require("null-ls").builtins.diagnostics.flake8,
         require("null-ls").builtins.formatting.gofmt
     },
 })
-
-
--- Debugger
-require'plugins.dap'
-require'dapui'.setup()
-require'dap-python'.setup('~/.venv/debugpy/bin/python')
-require'dap-go'.setup()
-vim.fn.sign_define("DapBreakpoint", { text = "", texthl = "Error", linehl = "", numhl = "" })
-vim.fn.sign_define("DapStopped", { text = "", texthl = "Success", linehl = "", numhl = "" })
