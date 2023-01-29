e9243a4834b0aa8de1898bc09cef5f6127dc6828
codico <codicocodes@gmail.com>
2023-01-29 20:54:43 +0100

    Add autocmd for CmdlineLeave

--- nvim/lua/codico/init.lua
+++ nvim/lua/codico/init.lua
@@ -5,6 +5,15 @@ require 'codico.keymaps'
 require'fidget'.setup {}
 require'mind'.setup {}
 require'Comment'.setup {}
+
 vim.api.nvim_create_autocmd("TextYankPost", {
-  command = "silent! lua vim.highlight.on_yank()",
+  callback = function ()
+    vim.highlight.on_yank()
+  end,
+})
+
+vim.api.nvim_create_autocmd("CmdlineLeave", {
+  callback = function ()
+    vim.defer_fn(function() vim.cmd('echo \"\"') end, 1000)
+  end
 })
