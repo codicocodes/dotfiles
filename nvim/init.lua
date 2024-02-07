509e768ec8404cbc898125ce556176908fd5ea8f
codico <codicocodes@gmail.com>
2024-02-07 21:02:19 +0100

    Shorten root path

--- nvim/init.lua
+++ nvim/init.lua
@@ -169,6 +169,16 @@ require("lazy").setup({
 				filtered_items = {
 					hide_dotfiles = false,
 				},
+				components = {
+					name = function(config, node, state)
+						local components = require('neo-tree.sources.common.components')
+						local name = components.name(config, node, state)
+						if node:get_depth() == 1 then
+							name.text = vim.fn.pathshorten(name.text, 2)
+						end
+						return name
+					end,
+				},
 			},
 		},
 		keys = {
